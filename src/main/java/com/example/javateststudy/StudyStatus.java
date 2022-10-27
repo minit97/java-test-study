e18d0808b99cddbe3331afd322daf1f350f65f36
박현민 <dndb4902@naver.com>
2022-10-27 17:23:08 +0900

    # Assertion

---
+++ src/main/java/com/example/javateststudy/StudyStatus.java
@@ -0,0 +1,5 @@
+package com.example.javateststudy;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/com/example/javateststudy/StudyTest.java
+++ src/test/java/com/example/javateststudy/StudyTest.java
@@ -2,20 +2,45 @@ package com.example.javateststudy;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)   // "_" -> " " 변경
 class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study study = new Study();
-        System.out.println("test1");
+        Study study = new Study(-10);
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
+                    @Override
+                    public String get() {
+                        return "스터디를 처음 만들면 DRAFT 상태다.";
+                    }
+                }),
+                () -> assertTrue(study.getLimit() > 0,"스터디 최대 참석 가능 인원은 0보다 커야 한다. ")
+        );
     }
 
     @Test
     void create_new_study_again() {
-        Study study = new Study();
-        System.out.println("test2");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        assertEquals("limit은 0보다 커야한다.", exception.getMessage());
+    }
+
+    @Test
+    void create_new_study2() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {   // Preemptively 는 기대한 값에 멈춤
+            new Study(10);
+            Thread.sleep(1000);
+        });
+        // ThreadLocal 쓴다면 예상치못한 결과가 나올 수 있다.
+        // 그렇기에 ThreadLocal이나 Thread와 관련없는 코드를 실행 시 괜찮다.
     }
 
     @BeforeAll
