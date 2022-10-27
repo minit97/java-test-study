2b5e85a6f9da9bea94f774add8ec29053a48d815
박현민 <dndb4902@naver.com>
2022-10-27 16:42:54 +0900

    # 기본애노테이션

--- src/test/java/com/example/javateststudy/StudyTest.java
+++ src/test/java/com/example/javateststudy/StudyTest.java
@@ -1,7 +1,38 @@
 package com.example.javateststudy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 class StudyTest {
 
+    @Test
+    void create() {
+        Study study = new Study();
+        System.out.println("test1");
+    }
+
+    @Test
+    void create1() {
+        Study study = new Study();
+        System.out.println("test2");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each");
+    }
 }
