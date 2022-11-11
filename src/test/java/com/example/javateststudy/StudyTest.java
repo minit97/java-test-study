939d97d6d24589feb9d8c9f4c8fc3cbcf60f49ec
박현민 <dndb4902@naver.com>
2022-11-11 13:56:31 +0900

    # 조건에 따라 테스트 실행하기

--- src/test/java/com/example/javateststudy/StudyTest.java
+++ src/test/java/com/example/javateststudy/StudyTest.java
@@ -1,11 +1,15 @@
 package com.example.javateststudy;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)   // "_" -> " " 변경
 class StudyTest {
@@ -14,13 +18,40 @@ class StudyTest {
     @DisplayName("assertj")
     void assertj() {
         Study actual = new Study(10);
-        org.assertj.core.api.Assertions.assertThat(actual.getLimit()).isGreaterThan(0);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("스터디 만들기3")
+    @EnabledOnOs({OS.MAC,OS.LINUX})
+    @EnabledOnJre({JRE.JAVA_8,JRE.JAVA_11})
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+    void create_new_study3() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("local");
+        Study actual = new Study(100);
+        assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
     @Test
     @DisplayName("스터디 만들기")
+    @DisabledOnOs(OS.MAC)
     void create_new_study() {
+
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));    // 테스트 환경이 로컬일때만 다음 코드를 실행한다.
         Study study = new Study(-10);
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            Study actual = new Study(100);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        }); // 조건에 따른 로직 수행
+
+        assumingThat("DEV".equalsIgnoreCase(test_env), () -> {
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        }); // 조건에 따른 로직 수행
 
         assertAll(
                 () -> assertNotNull(study),
